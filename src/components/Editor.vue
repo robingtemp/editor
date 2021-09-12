ecd4d97f2e1d89d24a34a9eb6018055b71066907
Robin Granqvist <54509721+robingranqvist@users.noreply.github.com>
2021-09-13 02:09:49 +0300

    Kmom02
    Kmom02

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
+        <div class="message">{{ message }}</div>
         <div class="toolbar">
-            <button v-on:click="onSave()" class="save">Spara</button>
+            <button v-on:click="onSave()" class="save">Spara nytt dokument</button>
+            <button v-on:click="onUpdate()" class="save">Uppdatera dokument</button>
         </div>
         <quillEditor 
             v-model="content"
             ref="quillEditor"
             :options="editorOptions"
         />
+        <h2>Ladda dokument</h2>
+        <ul class="content">
+            <li v-for="i in posts" :key="i._id" class="content-li">
+                <button v-on:click="loadPost(i._id)" class="save">{{ i._id }}</button>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 import 'quill/dist/quill.snow.css'
 import { quillEditor } from 'vue-quill-editor'
 // import { Button, Input, Select } from 'iview'
@@ -25,7 +34,10 @@ export default {
         quillEditor
     },
     data: () => ({
+        currentID: '',
         content: '',
+        message: '',
+        posts: {},
         editorOptions: {
             placeholder: 'Skriv nått här...',
             readOnly: false,
@@ -34,21 +46,46 @@ export default {
     }),
     methods: {
         onSave() {
-            console.log(this.content)
+            const text = { text: this.content }
+            axios
+            .post('http://localhost:1337/post', text)
+            .then(response => (this.message = response.data))
+            .catch((err) => this.message = err)
+        },
+        onUpdate() {
+            const text = { text: this.content }
+
+            axios
+            .patch('http://localhost:1337/update/' + this.currentID, text)
+            .then(response => (this.message = response.data))
+            .catch((err) => this.message = err)
+        },
+        loadPost(id) {
+            this.currentID = id
+            axios
+            .get('http://localhost:1337/get/' + id)
+            .then(response => this.content = response.data.text)
         }
+    },
+    mounted () {
+        axios
+        .get('http://localhost:1337/get')
+        .then(response => this.posts = response.data)
     }
 }
 </script>
 
 <style>
 .ql-editor {
-    height: 50vh;
+    height: 30vh;
 }
 
 .toolbar {
     border: 1px solid #ccc; 
     padding: 8px;
     margin-bottom: 8px;
+    display:flex;
+    justify-content: space-between;
 }
 
 .save {
@@ -61,5 +98,13 @@ export default {
 
 .save:hover {
     border: 1px solid #000;
+}
+
+.content {
+    padding: 0;
+}
+
+.content li {
+    list-style-type: none;
 }
 </style>
